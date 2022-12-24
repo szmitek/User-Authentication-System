8d9f9bfdf8ab316731bb834ca781ae2dc77298e9
Kamil S <61392709+szmitek@users.noreply.github.com>
2022-12-24 16:26:25 +0100

    :lock:  finished registerController for now

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,5 +1,48 @@
+const users = require("../server")
+const bcrypt = require('bcrypt')
+const crypto = require('crypto')
+
 exports.registerPage = (req, res) => {
     res.render('register.ejs')
 }
 
+exports.registerUser = async (req, res) => {
+    // Validate the user's input
+    if (!req.body.name || !req.body.email || !req.body.password) {
+        // If any of the required fields are missing, return an error
+        return res.status(400).send({ error: 'Name, email, and password are required' });
+    }
+    if (!/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(req.body.email)) {
+        // If the email is not in a valid format, return an error
+        return res.status(400).send({ error: 'Invalid email' });
+    }
 
+    // Check if a user with the same email already exists
+    const existingUser = users.find((user) => user.email === req.body.email);
+    if (existingUser) {
+        // If a user with the same email already exists, return an error
+        return res.status(400).send({ error: 'A user with this email already exists' });
+    }
+
+    try {
+        // Generate a unique salt for the new user
+        const salt = crypto.randomBytes(16).toString('hex');
+        // Hash the user's password with the unique salt
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        // Add the new user to the in-memory array
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+            salt,
+            loginAttempts: 0,
+            lockUntil: 0,
+        });
+        // Redirect the user to the login page
+        res.redirect('/login');
+    } catch (error) {
+        // If there was an error saving the user, return an error
+        res.status(500).send({ error: 'Error saving user' });
+    }
+};
--- server.js
+++ server.js
@@ -4,14 +4,16 @@ const app = express()
 const homeController = require('./controllers/homeController');
 const loginController = require('./controllers/loginController');
 const registerController = require('./controllers/registerController');
+const users = []
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', homeController.homePage)
 
 app.get('/login', loginController.loginPage)
 
 app.get('/register', registerController.registerPage)
-app.post('/register', registerController)
+app.post('/register', registerController.registerUser)
 
 app.listen(3000)
